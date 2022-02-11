d440c9ea9a722b069220f701b6df620675ce7e3e
algreenexo <algreenexo@gmail.com>
2022-02-11 15:24:39 +0200

    Add after login redirecting to /

--- src/hooks/routes.hook.tsx
+++ src/hooks/routes.hook.tsx
@@ -1,25 +1,27 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Navigate} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import TimeTablePage from "../pages/TimeTablePage";
 import BlogPage from "../pages/BlogPage";
 
 const useRoutes = (isAuthenticated: boolean) => {
-  if (isAuthenticated) {
-    return (
-      <Routes>
-        <Route path="/*" element={<HomePage/>}/>
-        <Route path="/timetable/*" element={<TimeTablePage/>}/>
-      </Routes>
-    );
-  }
+      if (isAuthenticated) {
+          return (
+              <Routes>
+                  <Route path="/" element={<HomePage/>}/>
+                  <Route path="/timetable/*" element={<TimeTablePage/>}/>
+                  <Route path="/*" element={<Navigate to={'/'}/>}/>
+              </Routes>
+          );
+      }
 
-  return (
-    <Routes>
-        <Route path="/*" element={<BlogPage/>}/>
-        <Route path="/login/*" element={<LoginPage/>}/>
-    </Routes>
-  );
+      return (
+          <Routes>
+              <Route path="/" element={<BlogPage/>}/>
+              <Route path="/login/*" element={<LoginPage/>}/>
+              <Route path="/*" element={<Navigate to={'/'}/>}/>
+          </Routes>
+      );
 }
 
 export default useRoutes;
